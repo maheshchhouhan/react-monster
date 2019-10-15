b4d909781a42882db1baf19e09567bce21257504
Mahesh Singh Chouhan <mahesh@futureshirts.com>
2019-10-15 19:53:50 +0530

    pushing build

---
+++ build/precache-manifest.0f65186a50f7d58cca646b0d398a2d5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e177cf950e93d52473ccbe6354070e88",
+    "url": "/maheshchhouhan/react-monster/index.html"
+  },
+  {
+    "revision": "de37d37996c68290cbb5",
+    "url": "/maheshchhouhan/react-monster/static/css/main.c87b2398.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/maheshchhouhan/react-monster/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "de37d37996c68290cbb5",
+    "url": "/maheshchhouhan/react-monster/static/js/main.b172a717.chunk.js"
+  },
+  {
+    "revision": "343ba8cd3d19304bdb67",
+    "url": "/maheshchhouhan/react-monster/static/js/runtime-main.ad0e2e94.js"
+  }
+]);
